6c3d5d25e5798a37bb5d4f29cafe613ca632c5a4
FelipeBelusso <felipeprestesbelusso@gmail.com>
2026-02-10 10:17:06 -0300

    Create atividade aula 2.py

---
+++ Programação-BackEnd/atividade aula 2.py
@@ -0,0 +1,12 @@
+Carros = ("Digite quantos carros dalessandro possui")
+#variavel carros recebe o que for digitado
+Note = ("Digite quantos notebooks Ibo tem")
+40
+#variavel Note recebe o que for digitado
+#Lucas comprou 10% dos notes e 50% dos carros, com quantos itens cada um deles ficou?
+# 50% = *0,5
+# 10% = *0,10
+Dalessandro = Carros * 0,5
+#variavel Dalessandro recebe carros vezes 0,5
+print(f"Dalessandro possui {Dalessandro} itens")
+#print utilizando "f" para poder inprimir texto e variavel no mesmo comando
