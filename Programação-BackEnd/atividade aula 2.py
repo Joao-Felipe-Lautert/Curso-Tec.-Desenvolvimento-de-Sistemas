4bf388f664f3e694bd3c7482b17a1923c06bf2d3
FelipeBelusso <felipeprestesbelusso@gmail.com>
2026-02-10 10:32:41 -0300

    Update atividade aula 2.py

--- Programação-BackEnd/atividade aula 2.py
+++ Programação-BackEnd/atividade aula 2.py
@@ -1,12 +1,13 @@
-Carros = ("Digite quantos carros dalessandro possui")
+Carros = int(input("Digite quantos carros Dalessandro possui \n"))
 #variavel carros recebe o que for digitado
-Note = ("Digite quantos notebooks Ibo tem")
-40
+Note = int(input("Digite quantos notebooks Ivo tem \n"))
 #variavel Note recebe o que for digitado
 #Lucas comprou 10% dos notes e 50% dos carros, com quantos itens cada um deles ficou?
 # 50% = *0,5
 # 10% = *0,10
-Dalessandro = Carros * 0,5
+Dalessandro = (Note * 0.9) + (Carros * 0.5)
 #variavel Dalessandro recebe carros vezes 0,5
 print(f"Dalessandro possui {Dalessandro} itens")
 #print utilizando "f" para poder inprimir texto e variavel no mesmo comando
+Lucas = (Note * 0.1) + (Carros * 0.5)
+print(f"Lucas possui {Lucas} itens")
