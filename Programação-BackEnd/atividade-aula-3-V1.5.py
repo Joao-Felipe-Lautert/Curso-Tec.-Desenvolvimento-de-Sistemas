7474f0c40fc097607799b2c9ee576421601c628f
FelipeBelusso <felipeprestesbelusso@gmail.com>
2026-02-24 11:06:20 -0300

    Add files via upload

---
+++ Programação-BackEnd/atividade-aula-3-V1.5.py
@@ -0,0 +1,45 @@
+#Crie um programa que receba 2 números, some-os, e retorne o resultado ao Usúario
+#Desáfio proposto pelo Professor Douglas Scariot, na matéria de Programação Back-End
+
+#Função que garante a execução continua do código (Código executa eternamente)
+def perguntas():
+    while True:
+        #Printa "-" 30 vezes para divisão de cada loop do código rodado
+        print("-" *148)
+        try:
+            #Inserção de dados por meio de perguntas ao usuário
+            input1 = float(input("Digite o 1º Número: \n"))
+            input2 = float(input("Digite o 2º Número: \n"))
+            print("Operações Possíveis: + | - | * | /")
+            operacao = str(input("Digite a operação desejada: \n"))
+            
+            #multiplica o número por 0.1* (10%)
+            numero1 = input1 * 0.1
+            numero2 = input2 * 0.1
+
+            #Cadeia de Cálculo
+            if operacao == "+":
+                resultadoSoma = float(numero1 + numero2)
+                print(f"O resuldado da soma é: {resultadoSoma}")
+            elif operacao == "-":
+                resultadoSub = float(numero1 - numero2)
+                print(f"O resuldado da soma é: {resultadoSub}")
+            elif operacao =="*":
+                resultadoMult = float(numero1 * numero2)
+                print(f"O resuldado da soma é: {resultadoMult}")
+            elif operacao == "/":
+                resultadoDiv = float(numero1 / numero2)
+                print(f"O resuldado da soma é: {resultadoDiv}")
+
+            else:
+                print("Operação Indefinida. Voltando ao início")
+                #Retorna para o início do Loop While em caso de Operação Indefinida
+                continue
+        #Retorna para o início do Loop While em caso de valores não numéricos
+        except ValueError:
+            print("Insira valores validos")
+
+#Chama a função para ser executada
+perguntas()
+
+#Para parar o programa, exclua o terminal no qual ele está rodando
