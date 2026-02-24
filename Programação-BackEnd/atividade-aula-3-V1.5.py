ff3705d3367ff18bf4f78bb18263cd1213a8f8ce
JoãoFelipeLautert <joao.lautert@outlook.com>
2026-02-24 11:50:09 -0300

    Enhance calculator with additional operations

--- Programação-BackEnd/atividade-aula-3-V1.5.py
+++ Programação-BackEnd/atividade-aula-3-V1.5.py
@@ -1,45 +1,58 @@
 #Crie um programa que receba 2 números, some-os, e retorne o resultado ao Usúario
 #Desáfio proposto pelo Professor Douglas Scariot, na matéria de Programação Back-End
 
+import math
+
 #Função que garante a execução continua do código (Código executa eternamente)
 def perguntas():
     while True:
         #Printa "-" 30 vezes para divisão de cada loop do código rodado
-        print("-" *148)
+        print("-" * 148)
         try:
             #Inserção de dados por meio de perguntas ao usuário
-            input1 = float(input("Digite o 1º Número: \n"))
-            input2 = float(input("Digite o 2º Número: \n"))
-            print("Operações Possíveis: + | - | * | /")
+            numero1 = float(input("Digite o 1º Número: \n"))
+            numero2 = float(input("Digite o 2º Número: \n"))
+            print("Operações Possíveis: + | - | * | / | ^ | raiz | % | !")
             operacao = str(input("Digite a operação desejada: \n"))
-            
-            #multiplica o número por 0.1* (10%)
-            numero1 = input1 * 0.1
-            numero2 = input2 * 0.1
 
             #Cadeia de Cálculo
             if operacao == "+":
-                resultadoSoma = float(numero1 + numero2)
-                print(f"O resuldado da soma é: {resultadoSoma}")
+                resultado = numero1 + numero2
+                print(f"O resuldado da soma é: {resultado}")
             elif operacao == "-":
-                resultadoSub = float(numero1 - numero2)
-                print(f"O resuldado da soma é: {resultadoSub}")
+                resultado = numero1 - numero2
+                print(f"O resuldado da subtração é: {resultado}")
             elif operacao =="*":
-                resultadoMult = float(numero1 * numero2)
-                print(f"O resuldado da soma é: {resultadoMult}")
+                resultado = numero1 * numero2
+                print(f"O resuldado da multiplicação é: {resultado}")
             elif operacao == "/":
-                resultadoDiv = float(numero1 / numero2)
-                print(f"O resuldado da soma é: {resultadoDiv}")
-
+                resultado = numero1 / numero2
+                print(f"O resuldado da divisão é: {resultado}")
+            elif operacao == "^":
+                resultado = numero1 ** numero2
+                print(f"O resultado da Potencia é {resultado}")
+            elif operacao == "raiz":
+                resultado = numero1 ** (1/2)
+                resultado1 = numero2 ** (1/2)
+                print(f"O resultado da raiz do 1º número é {resultado} e do 2º número é {resultado1}")
+            elif operacao == "%":
+                resultado = numero1 * (numero2 / 100)
+                print(f"O resultado da porcentagem do 2º número sobre o 1º número é {resultado}")
+            elif operacao == "!":
+                numero1int = int(numero1)
+                numero2int = int(numero2)
+                resultado = math.factorial(numero1int)
+                resultado1 = math.factorial(numero2int)
+                print(f"O resultado da fotorial do 1º número é {resultado} e do 2º número {resultado1}")
             else:
                 print("Operação Indefinida. Voltando ao início")
                 #Retorna para o início do Loop While em caso de Operação Indefinida
                 continue
+            
         #Retorna para o início do Loop While em caso de valores não numéricos
         except ValueError:
             print("Insira valores validos")
 
 #Chama a função para ser executada
 perguntas()
-
 #Para parar o programa, exclua o terminal no qual ele está rodando
