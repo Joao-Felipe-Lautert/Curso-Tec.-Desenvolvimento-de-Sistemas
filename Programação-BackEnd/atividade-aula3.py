e223db0e4d0e53f14e2d428fb57f0d1d26ad515a
JoãoFelipeLautert <joao.lautert@outlook.com>
2026-02-24 07:45:52 -0300

    Update calculator to loop and handle invalid input
    Refactor calculator program to run continuously and handle errors.

--- Programação-BackEnd/atividade-aula3.py
+++ Programação-BackEnd/atividade-aula3.py
@@ -1,26 +1,39 @@
 #Crie um programa que receba 2 números, some-os, e retorne o resultado ao Usúario
+#Desáfio proposto pelo Professor Douglas Scariot, na matéria de Programação Back-End
+
+#Função que garante a execução continua do código (Código executa eternamente)
 def perguntas():
-    numero1 = float(input("Digite o 1º Número: \n"))
+    while True:
+        #Printa "-" 30 vezes para divisão de cada loop do código rodado
+        print("-" *30)
+        try:
+            #Inserção de dados por meio de perguntas ao usuário
+            numero1 = float(input("Digite o 1º Número: \n"))
+            numero2 = float(input("Digite o 2º Número: \n"))
+            print("Operações Possíveis: + | - | * | /")
+            operacao = str(input("Digite a operação desejada: \n"))
+            #Cadeia de Cálculo
+            if operacao == "+":
+                resultadoSoma = float(numero1 + numero2)
+                print(f"O resuldado da soma é: {resultadoSoma}")
+            elif operacao == "-":
+                resultadoSub = float(numero1 - numero2)
+                print(f"O resuldado da soma é: {resultadoSub}")
+            elif operacao =="*":
+                resultadoMult = float(numero1 * numero2)
+                print(f"O resuldado da soma é: {resultadoMult}")
+            elif operacao == "/":
+                resultadoDiv = float(numero1 / numero2)
+                print(f"O resuldado da soma é: {resultadoDiv}")
+            else:
+                print("Operação Indefinida. Voltando ao início")
+                #Retorna para o início do Loop While em caso de Operação Indefinida
+                continue
+        #Retorna para o início do Loop While em caso de valores não numéricos
+        except ValueError:
+            print("Insira valores validos")
 
-    numero2 = float(input("Digite o 2º Número: \n"))
+#Chama a função para ser executada
+perguntas()
 
-    print("Operações Possíveis: + - * /")
-    operacao = str(input("Digite a operação desejada: \n"))
-    return numero1, numero2, operacao
-numero1, numero2, operacao = perguntas()
-
-if operacao == "+":
-    resultadoSoma = float(numero1 + numero2)
-    print(f"O resuldado da soma é: {resultadoSoma}")
-elif operacao == "-":
-    resultadoSub = float(numero1 - numero2)
-    print(f"O resuldado da subtração é: {resultadoSub}")
-elif operacao =="*":
-    resultadoMult = float(numero1 * numero2)
-    print(f"O resuldado da multiplicação é: {resultadoMult}")
-elif operacao == "/":
-    resultadoDiv = float(numero1 / numero2)
-    print(f"O resuldado da divisão é: {resultadoDiv}")
-else:
-    print("Operação Indefinida. Voltando ao início")
-    perguntas()
+#Para parar o programa, exclua o terminal no qual ele está rodando
